53bf8175289924668fccb8449c8d542503eb58b3
GyovanneMS <98404327+GyovanneMS@users.noreply.github.com>
2022-05-09 09:41:07 -0300

    Add files via upload

--- SomaMatrizes.java
+++ SomaMatrizes.java
@@ -6,9 +6,44 @@ public class SomaMatrizes {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		
-		double matriz1[][] = {{54, 47},{2, 10},{8, 13}};
-		double matriz2[][] = {{13, 8},{10, 2},{47, 54}};
+		
+		
+		double matriz1[][] = new double[3][2];
+		Fixar(matriz1);
+		double matriz2[][] = new double[3][2];
+		Fixar(matriz2);
 		double matriz3[][] = new double[3][2];
+		SomarM(matriz1, matriz2, matriz3);
+		
+
+		for(int linha=0; linha<matriz3.length; linha++) {
+			for(int coluna=0; coluna<matriz3[0].length; coluna++) {
+				System.out.print(matriz3[linha][coluna] + "|");
+				if(coluna == 3) {
+					System.out.println();
+				}
+			}	
+		}
 	}
+	public static void SomarM(double matriz1[][], double matriz2[][], double matriz3[][]) {
+		for(int linha=0; linha<matriz3.length; linha++) {
+			for(int coluna=0; coluna<matriz3[0].length; coluna++) {
+				matriz3[linha][coluna] = matriz1[linha][coluna] + matriz2[linha][coluna];
+			}	
+		}
+	}
+	public static void Fixar(double matriz[][]) {
+		Scanner ler = new Scanner(System.in);
+		
+		for(int linha=0; linha<matriz.length; linha++) {
+			for(int coluna=0; coluna<matriz[0].length; coluna++) {
+				System.out.print("Escolha um número: ");
+				matriz[linha][coluna] =  ler.nextDouble();	
+			}
+		}
+	}
+	
 }
+
+
 		
